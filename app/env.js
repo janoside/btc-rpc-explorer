8b812faced47e03a0a1d43855dbcf29a4f27a79d
rootSig <34191200+rootSig@users.noreply.github.com>
2018-03-19 18:40:57 +0700

    BCA related changes

--- app/env.js
+++ app/env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	cookiePassword: "0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-	debug: false,
+	debug: true,
 
 	rpcBlacklist:[
 		"stop"
@@ -9,12 +9,12 @@ module.exports = {
 	// Uncomment "bitcoind" below to automatically connect via RPC.
 	// Otherwise, you can manually connect via the UI.
 
-	//bitcoind:{
-	//	host:"192.168.1.100",
-	//	port:8332,
-	//	rpc: {
-	//		username:"username",
-	//		password:"password"
-	//	}
-	//}
+	bitcoind:{
+		host:"127.0.0.1",
+		port:17332,
+		rpc: {
+			username:"123",
+			password:"123"
+		}
+	}
 };
