0ca44fb676ef890d013f9fb60e9337437f1fb738
Dan Janosik <janoside@gmail.com>
2018-05-10 00:56:43 -0400

    style tweaks

--- app/coins/btc.js
+++ app/coins/btc.js
@@ -4,7 +4,7 @@ Decimal8 = Decimal.clone({ precision:8, rounding:8 });
 module.exports = {
 	name:"Bitcoin",
 	logoUrl:"/img/logo/btc.svg",
-	siteTitle:"BTC RPC Explorer",
+	siteTitle:"Bitcoin Explorer",
 	siteDescriptionHtml:"<b>BTC Explorer</b> is <a href='https://github.com/janoside/btc-rpc-explorer). If you run your own [Bitcoin Full Node](https://bitcoin.org/en/full-node), **BTC Explorer** can easily run alongside it, communicating via RPC calls. See the project [ReadMe](https://github.com/janoside/btc-rpc-explorer) for a list of features and instructions for running.",
 	nodeTitle:"Bitcoin Full Node",
 	nodeUrl:"https://bitcoin.org/en/full-node",
