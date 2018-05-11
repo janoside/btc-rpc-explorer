47773f99d46ddf468e701fd17f8b864fef51e1e0
Dan Janosik <janoside@gmail.com>
2018-05-11 13:38:20 -0400

    cleanup

--- app/env.js
+++ app/env.js
@@ -1,7 +1,6 @@
 module.exports = {
 	cookiePassword: "0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
 	demoSite: true,
-	showForkBanner: false,
 	coin: "BTC",
 
 	rpcBlacklist:[
