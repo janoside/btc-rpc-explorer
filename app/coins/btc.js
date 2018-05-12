f981b4bddf19f808ec41c227abd16d86d6af1764
Dan Janosik <janoside@gmail.com>
2018-05-12 15:35:36 -0400

    simpler demo-site urls

--- app/coins/btc.js
+++ app/coins/btc.js
@@ -8,7 +8,7 @@ module.exports = {
 	siteDescriptionHtml:"<b>BTC Explorer</b> is <a href='https://github.com/janoside/btc-rpc-explorer). If you run your own [Bitcoin Full Node](https://bitcoin.org/en/full-node), **BTC Explorer** can easily run alongside it, communicating via RPC calls. See the project [ReadMe](https://github.com/janoside/btc-rpc-explorer) for a list of features and instructions for running.",
 	nodeTitle:"Bitcoin Full Node",
 	nodeUrl:"https://bitcoin.org/en/full-node",
-	demoSiteUrl: "https://btc-explorer.chaintools.io",
+	demoSiteUrl: "https://btc.chaintools.io",
 	currencyUnits:[
 		{
 			name:"BTC",
