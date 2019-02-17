d3ef035d008ebb64bc75e3a1bc699fdd6fbe117d
Dan Janosik <dan@47.io>
2019-02-17 00:15:09 -0500

    Attempt to address #103 using the hypothesis that css/js files may be modified (minified?) before being hashed in some situations

--- app.js
+++ app.js
@@ -71,6 +71,15 @@ app.use(session({
 	resave: false,
 	saveUninitialized: false
 }));
+
+app.use(function(req, res, next) {
+	if (req.url.match(/^.*(css|js)$/)) {
+		res.setHeader("Cache-Control", "no-transform");
+	}
+
+	next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 process.on("unhandledRejection", (reason, p) => {
