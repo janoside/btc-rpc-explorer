a3db455321153d6e7ac8c5e72e7f67e288a2e724
Andrea Suisani <sickpig@gmail.com>
2020-01-20 12:33:13 +0100

    Rename btc.js to bch.js and made necessary modification

--- app/coins.js
+++ app/coins.js
@@ -1,9 +1,7 @@
-var btc = require("./coins/btc.js");
-var ltc = require("./coins/ltc.js");
+var bch = require("./coins/bch.js");
 
 module.exports = {
-	"BTC": btc,
-	"LTC": ltc,
+  "BCH": bch,
 
-	"coins":["BTC", "LTC"]
+  "coins":["BCH"]
 };
