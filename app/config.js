dd04bd86037c23cb366d19bc04ed3281b531d30a
Andrea Suisani <sickpig@gmail.com>
2020-02-04 15:51:38 +0100

    Actually set BCH as the default ticker (#8)

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ var url = require('url');
 var coins = require("./coins.js");
 var credentials = require("./credentials.js");
 
-var currentCoin = process.env.BTCEXP_COIN || "BTC";
+var currentCoin = process.env.BTCEXP_COIN || "BCH";
 
 var rpcCred = credentials.rpc;
 
