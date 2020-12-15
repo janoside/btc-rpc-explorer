23a4d88961c784730d4f6c4480d80fb78e180e34
Dan Janosik <dan@47.io>
2020-12-15 13:46:51 -0500

    more mining id config work using new repo

--- app/coins/btc.js
+++ app/coins/btc.js
@@ -69,7 +69,7 @@ module.exports = {
 	nodeUrl:"https://bitcoin.org/en/full-node",
 	demoSiteUrl: "https://explorer.btc21.org",
 	miningPoolsConfigUrls:[
-		"https://raw.githubusercontent.com/btc21/Bitcoin-Mining-Pools/master/pools.json",
+		"https://raw.githubusercontent.com/btc21/Bitcoin-Known-Miners/master/miners.json",
 		"https://raw.githubusercontent.com/btccom/Blockchain-Known-Pools/master/pools.json",
 		"https://raw.githubusercontent.com/blockchain/Blockchain-Known-Pools/master/pools.json"
 	],
