4417070dc1ea2a4e287fec7f7baa480236303287
Nadav Ivgi <nadav@shesek.info>
2021-02-04 02:20:05 +0200

    Set one error handler only
    Running both one after the other results in:
    
       Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client

--- app.js
+++ app.js
@@ -637,17 +637,17 @@ if (app.get('env') === 'development') {
 			error: err
 		});
 	});
-}
-
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
+} else {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: {}
+		});
 	});
-});
+}
 
 app.locals.moment = moment;
 app.locals.Decimal = Decimal;
