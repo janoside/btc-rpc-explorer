7bea8d616222f6ca8a389b15936516bdca94ffdf
Dan Janosik <dan@47.io>
2021-04-02 16:03:04 -0400

    WIP: major mempool summary perf improvements
    Heavy caching of mempool summary will make subsequent loads MUCH faster with low memory overhead. Future improvement could be to pre-cache for non-slow devices.
    
    This is PoC. Still need some cache management stuff

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -14,6 +14,7 @@ const bitcoinjs = require('bitcoinjs-lib');
 const sha256 = require("crypto-js/sha256");
 const hexEnc = require("crypto-js/enc-hex");
 const Decimal = require("decimal.js");
+const asyncHandler = require("express-async-handler");
 
 const utils = require('./../app/utils.js');
 const coins = require("./../app/coins.js");
@@ -90,6 +91,291 @@ router.get("/mempool-txs/:txids", function(req, res, next) {
 		next();
 	});
 });
+
+const mempoolTxSummaryCache = {};
+
+
+router.get("/mempool-summary", asyncHandler(async (req, res, next) => {
+	try {
+		const txids = await utils.timePromise("promises.mempool-summary.getAllMempoolTxids", coreApi.getAllMempoolTxids());
+
+		const promises = [];
+		const results = [];
+
+		for (var i = 0; i < txids.length; i++) {
+			const txid = txids[i];
+			const key = txid.substring(0, 6);
+
+			if (mempoolTxSummaryCache[key]) {
+				results.push(mempoolTxSummaryCache[key]);
+
+			} else {
+				promises.push(new Promise(async (resolve, reject) => {
+					try {
+						const item = await coreApi.getMempoolTxDetails(txid, false);
+						const itemSummary = {
+							f: item.entry.fees.modified,
+							sz: item.entry.vsize ? item.entry.vsize : item.entry.size,
+							af: item.entry.fees.ancestor,
+							df: item.entry.fees.descendant,
+							t: item.entry.time
+						};
+
+						mempoolTxSummaryCache[key] = itemSummary;
+
+						results.push(mempoolTxSummaryCache[key]);
+						
+						resolve();
+
+					} catch (e) {
+						// don't care
+						resolve();
+					}
+				}));
+			}
+		}
+
+		await Promise.all(promises);
+
+
+		var summary = [];
+
+		var maxFee = 0;
+		var maxFeePerByte = 0;
+		var maxAge = 0;
+		var maxSize = 0;
+		var ages = [];
+		var sizes = [];
+
+		for (var i = 0; i < results.length; i++) {
+			var txMempoolInfo = results[i];
+
+			var fee = txMempoolInfo.f;
+			var size = txMempoolInfo.sz;
+			var feePerByte = txMempoolInfo.f / size;
+			var age = Date.now() / 1000 - txMempoolInfo.t;
+
+			if (fee > maxFee) {
+				maxFee = fee;
+			}
+
+			if (feePerByte > maxFeePerByte) {
+				maxFeePerByte = feePerByte;
+			}
+
+			ages.push({age:age, txid:"abc"});
+			sizes.push({size:size, txid:"abc"});
+
+			if (age > maxAge) {
+				maxAge = age;
+			}
+
+			if (size > maxSize) {
+				maxSize = size;
+			}
+		}
+
+		ages.sort(function(a, b) {
+			if (a.age != b.age) {
+				return b.age - a.age;
+
+			} else {
+				return a.txid.localeCompare(b.txid);
+			}
+		});
+
+		sizes.sort(function(a, b) {
+			if (a.size != b.size) {
+				return b.size - a.size;
+
+			} else {
+				return a.txid.localeCompare(b.txid);
+			}
+		});
+
+		maxSize = 2000;
+
+		const satoshiPerByteBucketMaxima = coinConfig.feeSatoshiPerByteBucketMaxima;
+
+		var bucketCount = satoshiPerByteBucketMaxima.length + 1;
+
+		var satoshiPerByteBuckets = [];
+		var satoshiPerByteBucketLabels = [];
+
+		satoshiPerByteBucketLabels[0] = ("[0 - " + satoshiPerByteBucketMaxima[0] + ")");
+		for (var i = 0; i < bucketCount; i++) {
+			satoshiPerByteBuckets[i] = {"count":0, "totalFees":0, "totalBytes":0};
+
+			if (i > 0 && i < bucketCount - 1) {
+				satoshiPerByteBucketLabels[i] = ("[" + satoshiPerByteBucketMaxima[i - 1] + " - " + satoshiPerByteBucketMaxima[i] + ")");
+			}
+		}
+
+		var ageBucketCount = 150;
+		var ageBucketTxCounts = [];
+		var ageBucketLabels = [];
+
+		var sizeBucketCount = 150;
+		var sizeBucketTxCounts = [];
+		var sizeBucketLabels = [];
+
+		for (var i = 0; i < ageBucketCount; i++) {
+			var rangeMin = i * maxAge / ageBucketCount;
+			var rangeMax = (i + 1) * maxAge / ageBucketCount;
+
+			ageBucketTxCounts.push(0);
+
+			if (maxAge > 600) {
+				var rangeMinutesMin = new Decimal(rangeMin / 60).toFixed(1);
+				var rangeMinutesMax = new Decimal(rangeMax / 60).toFixed(1);
+
+				ageBucketLabels.push(rangeMinutesMin + " - " + rangeMinutesMax + " min");
+
+			} else {
+				ageBucketLabels.push(parseInt(rangeMin) + " - " + parseInt(rangeMax) + " sec");
+			}
+		}
+
+		for (var i = 0; i < sizeBucketCount; i++) {
+			sizeBucketTxCounts.push(0);
+
+			if (i == sizeBucketCount - 1) {
+				sizeBucketLabels.push(parseInt(i * maxSize / sizeBucketCount) + "+");
+
+			} else {
+				sizeBucketLabels.push(parseInt(i * maxSize / sizeBucketCount) + " - " + parseInt((i + 1) * maxSize / sizeBucketCount));
+			}
+		}
+
+		satoshiPerByteBucketLabels[bucketCount - 1] = (satoshiPerByteBucketMaxima[satoshiPerByteBucketMaxima.length - 1] + "+");
+
+		var summary = {
+			"count":0,
+			"totalFees":0,
+			"totalBytes":0,
+			"satoshiPerByteBuckets":satoshiPerByteBuckets,
+			"satoshiPerByteBucketLabels":satoshiPerByteBucketLabels,
+			"ageBucketTxCounts":ageBucketTxCounts,
+			"ageBucketLabels":ageBucketLabels,
+			"sizeBucketTxCounts":sizeBucketTxCounts,
+			"sizeBucketLabels":sizeBucketLabels
+		};
+
+		for (var x = 0; x < results.length; x++) {
+			var txMempoolInfo = results[x];
+			var fee = txMempoolInfo.f;
+			var size = txMempoolInfo.sz;
+			var feePerByte = txMempoolInfo.f / size;
+			var satoshiPerByte = feePerByte * 100000000; // TODO: magic number - replace with coinConfig.baseCurrencyUnit.multiplier
+			var age = Date.now() / 1000 - txMempoolInfo.t;
+
+			var addedToBucket = false;
+			for (var i = 0; i < satoshiPerByteBucketMaxima.length; i++) {
+				if (satoshiPerByteBucketMaxima[i] > satoshiPerByte) {
+					satoshiPerByteBuckets[i]["count"]++;
+					satoshiPerByteBuckets[i]["totalFees"] += fee;
+					satoshiPerByteBuckets[i]["totalBytes"] += size;
+
+					addedToBucket = true;
+
+					break;
+				}
+			}
+
+			if (!addedToBucket) {
+				satoshiPerByteBuckets[bucketCount - 1]["count"]++;
+				satoshiPerByteBuckets[bucketCount - 1]["totalFees"] += fee;
+				satoshiPerByteBuckets[bucketCount - 1]["totalBytes"] += size;
+			}
+
+			summary["count"]++;
+			summary["totalFees"] += fee;
+			summary["totalBytes"] += size;
+
+			var ageBucketIndex = Math.min(ageBucketCount - 1, parseInt(age / (maxAge / ageBucketCount)));
+			var sizeBucketIndex = Math.min(sizeBucketCount - 1, parseInt(size / (maxSize / sizeBucketCount)));
+
+			ageBucketTxCounts[ageBucketIndex]++;
+			sizeBucketTxCounts[sizeBucketIndex]++;
+		}
+
+		summary["averageFee"] = summary["totalFees"] / summary["count"];
+		summary["averageFeePerByte"] = summary["totalFees"] / summary["totalBytes"];
+
+		summary["satoshiPerByteBucketMaxima"] = satoshiPerByteBucketMaxima;
+		summary["satoshiPerByteBucketCounts"] = [];
+		summary["satoshiPerByteBucketTotalFees"] = [];
+
+		for (var i = 0; i < bucketCount; i++) {
+			summary["satoshiPerByteBucketCounts"].push(summary["satoshiPerByteBuckets"][i]["count"]);
+			summary["satoshiPerByteBucketTotalFees"].push(summary["satoshiPerByteBuckets"][i]["totalFees"]);
+		}
+
+		res.json(summary);
+
+		next();
+
+	} catch (err) {
+		utils.logError("329r7whegee", err);
+
+		res.json({success:false, error:err});
+
+		next();
+	}
+}));
+
+router.get("/mempool-tx-summaries/:txids", asyncHandler(async (req, res, next) => {
+	try {
+		const txids = req.params.txids.split(",").map(utils.asHash);
+
+		const promises = [];
+		const results = [];
+
+		for (var i = 0; i < txids.length; i++) {
+			const txid = txids[i];
+			const key = txid.substring(0, 6);
+
+			if (mempoolTxSummaryCache[key]) {
+				results.push(mempoolTxSummaryCache[key]);
+
+			} else {
+				promises.push(new Promise(async (resolve, reject) => {
+					try {
+						const item = await coreApi.getMempoolTxDetails(txid, false);
+						const itemSummary = {
+							f: item.entry.fees.modified,
+							sz: item.entry.vsize ? item.entry.vsize : item.entry.size,
+							af: item.entry.fees.ancestor,
+							df: item.entry.fees.descendant,
+							t: item.entry.time
+						};
+
+						mempoolTxSummaryCache[key] = itemSummary;
+
+						results.push(mempoolTxSummaryCache[key]);
+						
+						resolve();
+
+					} catch (e) {
+						// don't care
+						resolve();
+					}
+				}));
+			}
+		}
+
+		await Promise.all(promises);
+
+		res.json(results);
+
+		next();
+
+	} catch (err) {
+		res.json({success:false, error:err});
+
+		next();
+	}
+}));
 
 router.get("/raw-tx-with-inputs/:txid", function(req, res, next) {
 	var txid = utils.asHash(req.params.txid);
