1da895a43da8b69c1db69beeb71032709b7d2da3
Dan Janosik <dan@47.io>
2021-05-21 10:54:05 -0400

    tweaks to public api:
    -now separated from internal api
    -/api/docs instead of static markdown file
    -tweak a few actions

---
+++ docs/api.js
@@ -0,0 +1,131 @@
+module.exports = {
+	"routes":[
+		// blocks
+		{
+			"category":"blocks",
+			"url":"/api/block/:hash",
+			"desc":"Returns the details of a block by hash.",
+			"returnType":"json",
+			"testUrl":"/api/block/0000000000000000001c8018d9cb3b742ef25114f27563e3fc4a1902167f9893"
+		},
+
+		{
+			"category":"blocks",
+			"url":"/api/block/:height",
+			"desc":"Returns the details of a block by height.",
+			"returnType":"json",
+			"testUrl":"/api/block/123456"
+		},
+
+		{
+			"category":"blocks",
+			"url":"/api/blocks/tip/height",
+			"desc":"Returns the height of the chain tip.",
+			"returnType":"integer"
+		},
+
+		{
+			"category":"blocks",
+			"url":"/api/blocks/tip/hash",
+			"desc":"Returns the block hash of the chain tip.",
+			"returnType":"string"
+		},
+
+
+
+
+		// transactions
+		{
+			"category":"transactions",
+			"url":"/api/tx/:txid",
+			"desc":"Returns the details of a transaction.",
+			"returnType":"json",
+			"testUrl": "/api/tx/f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16"
+		},
+
+
+
+		// blockchain
+		{
+			"category":"blockchain",
+			"url":"/api/blockchain/coins",
+			"desc":"Returns the current supply of Bitcoin. An estimate using a checkpoint can be returned in 2 cases: on 'slow' devices, and before the UTXO set summary is loaded.",
+			"returnType":"number"
+		},
+
+
+
+
+		// mining
+		{
+			"category":"mining",
+			"url":"/api/mining/hashrate",
+			"desc":"Returns the network hash rate, estimated over the last 1, 7, and 30 days.",
+			"returnType":"json",
+			"example": {"1Day":174.1,"7Day":165.9,"30Day":158.5, "unit":"exa"}
+		},
+
+		{
+			"category":"mining",
+			"url":"/api/mining/diff-adj-estimate",
+			"desc":"Returns the current estimate for the next difficulty adjustment as a percentage.",
+			"returnType":"number"
+		},
+
+
+
+		// mempool
+		{
+			"category":"mempool",
+			"url":"/api/mempool/count",
+			"desc":"Returns the number of transactions in Bitcoin Core's mempool.",
+			"returnType":"integer"
+		},
+
+		{
+			"category":"mempool",
+			"url":"/api/mempool/fees",
+			"desc":"Returns recommended fee rates in sats/vB for next block, ~30 min, 1 hr, and 1 day.",
+			"returnType":"json",
+			"example": {"nextBlock":17,"30min":9,"60min":9,"1day":9}
+		},
+
+
+
+		// price
+		{
+			"category":"price",
+			"url":"/api/price",
+			"desc":"Returns the price of 1 BTC, in USD, EUR, GBP, and XAU",
+			"returnType":"number"
+		},
+
+		{
+			"category":"price",
+			"url":"/api/price/:currency",
+			"desc":"Returns the price of 1 BTC, in one of USD, EUR, GBP, XAU",
+			"params":[{name: "currency", "options": ["usd", "eur", "gbp", "xau"]}],
+			"returnType":"number",
+			"testUrl": "/api/price/usd"
+		},
+
+		{
+			"category":"price",
+			"url":"/api/price/:currency/marketcap",
+			"desc":"Returns the market cap of Bitcoin, in one of USD, EUR, GBP, XAU",
+			"params":[{name: "currency", "options": ["usd", "eur", "gbp", "xau"]}],
+			"returnType":"number",
+			"testUrl": "/api/price/usd/marketcap"
+		},
+
+		{
+			"category":"price",
+			"url":"/api/price/:currency/moscowtime",
+			"desc":"Returns the price of 1 unit in 'currency' (e.g. $1) in satoshis",
+			"params":[{name: "currency", "options": ["usd", "eur", "gbp", "xau"]}],
+			"returnType":"number",
+			"testUrl": "/api/price/usd/moscowtime"
+		},
+
+	]
+}
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -22,277 +22,90 @@ const config = require("./../app/config.js");
 const coreApi = require("./../app/api/coreApi.js");
 const addressApi = require("./../app/api/addressApi.js");
 const rpcApi = require("./../app/api/rpcApi.js");
+const apiDocs = require("./../docs/api.js");
 
 const forceCsrf = csurf({ ignoreMethods: [] });
 
 
 
 
+router.get("/docs", function(req, res, next) {
+	res.locals.apiDocs = apiDocs;
+	res.locals.route = req.query.route;
 
-router.get("/blocks-by-height/:blockHeights", function(req, res, next) {
-	var blockHeightStrs = req.params.blockHeights.split(",");
-	
-	var blockHeights = [];
-	for (var i = 0; i < blockHeightStrs.length; i++) {
-		blockHeights.push(parseInt(blockHeightStrs[i]));
+	res.locals.categories = [];
+	apiDocs.routes.forEach(x => {
+		let category = x.category;
+
+		if (!res.locals.categories.find(y => (y.name == category))) {
+			res.locals.categories.push({name:category, items:[]});
+		}
+
+		res.locals.categories.find(x => (x.name == category)).items.push(x);
+	});
+
+	res.render("api-docs");
+
+	next();
+});
+
+
+
+
+
+
+/// BLOCKS
+
+router.get("/blocks/tip/height", asyncHandler(async (req, res, next) => {
+	try {
+		const blockcount = await rpcApi.getBlockCount();
+
+		res.send(blockcount.toString());
+
+	} catch (e) {
+		utils.logError("a39gfoeuew", e);
+
+		res.json({success: false});
 	}
 
-	coreApi.getBlocksByHeight(blockHeights).then(function(result) {
-		res.json(result);
+	next();
+}));
+
+router.get("/blocks/tip/hash", function(req, res, next) {
+	coreApi.getBlockchainInfo().then(function(getblockchaininfo){
+		res.send(getblockchaininfo.bestblockhash.toString());
 	}).catch(next);
 });
 
-router.get("/block-headers-by-height/:blockHeights", function(req, res, next) {
-	var blockHeightStrs = req.params.blockHeights.split(",");
-	
-	var blockHeights = [];
-	for (var i = 0; i < blockHeightStrs.length; i++) {
-		blockHeights.push(parseInt(blockHeightStrs[i]));
-	}
+router.get("/block/:hashOrHeight", asyncHandler(async (req, res, next) => {
+	const hashOrHeight = req.params.hashOrHeight;
+	let hash = (hashOrHeight.length == 64 ? hashOrHeight : null);
 
-	coreApi.getBlockHeadersByHeight(blockHeights).then(function(result) {
-		res.json(result);
-
-		next();
-	});
-});
-
-router.get("/block-stats-by-height/:blockHeights", function(req, res, next) {
-	var blockHeightStrs = req.params.blockHeights.split(",");
-	
-	var blockHeights = [];
-	for (var i = 0; i < blockHeightStrs.length; i++) {
-		blockHeights.push(parseInt(blockHeightStrs[i]));
-	}
-
-	coreApi.getBlocksStatsByHeight(blockHeights).then(function(result) {
-		res.json(result);
-
-		next();
-	});
-});
-
-router.get("/mempool-txs/:txids", function(req, res, next) {
-	var txids = req.params.txids.split(",").map(utils.asHash);
-
-	var promises = [];
-
-	for (var i = 0; i < txids.length; i++) {
-		promises.push(coreApi.getMempoolTxDetails(txids[i], false));
-	}
-
-	Promise.all(promises).then(function(results) {
-		res.json(results);
-
-		next();
-
-	}).catch(function(err) {
-		res.json({success:false, error:err});
-
-		next();
-	});
-});
-
-
-
-const mempoolSummaryStatuses = {};
-const mempoolSummaries = {};
-
-router.get("/mempool-summary-status", asyncHandler(async (req, res, next) => {
-	const statusId = req.query.statusId;
-	if (statusId && mempoolSummaryStatuses[statusId]) {
-		res.json(mempoolSummaryStatuses[statusId]);
-
-		next();
-
-	} else {
-		res.json({});
-
-		next();
-	}
-}));
-
-router.get("/get-mempool-summary", asyncHandler(async (req, res, next) => {
-	const statusId = req.query.statusId;
-
-	if (statusId && mempoolSummaries[statusId]) {
-		var summary = mempoolSummaries[statusId];
-		
-		res.json(summary);
-
-		next();
-
-		delete mempoolSummaries[statusId];
-		delete mempoolSummaryStatuses[statusId];
-
-	} else {
-		res.json({});
-
-		next();
-	}
-}));
-
-router.get("/build-mempool-summary", asyncHandler(async (req, res, next) => {
 	try {
-		// long timeout
-		res.connection.setTimeout(600000);
 
-
-		const statusId = req.query.statusId;
-		if (statusId) {
-			mempoolSummaryStatuses[statusId] = {};
+		if (hash == null) {
+			hash = await coreApi.getBlockHashByHeight(parseInt(hashOrHeight));
 		}
 
-		res.json({success:true, status:"started"});
+		const block = await coreApi.getBlockByHash(hash);
 
-		next();
+		res.json(block);
 
+	} catch (e) {
+		utils.logError("w9fgeddsuos", e);
 
-		const ageBuckets = req.query.ageBuckets ? parseInt(req.query.ageBuckets) : 100;
-		const sizeBuckets = req.query.sizeBuckets ? parseInt(req.query.sizeBuckets) : 100;
-
-
-		var summary = await coreApi.buildMempoolSummary(statusId, ageBuckets, sizeBuckets, (update) => {
-			mempoolSummaryStatuses[statusId] = update;
-		});
-
-		// store summary until it's retrieved via /api/get-mempool-summary
-		mempoolSummaries[statusId] = summary;
-
-	} catch (err) {
-		utils.logError("329r7whegee", err);
+		res.json({success: false});
 	}
+
+	next();
 }));
 
 
 
 
-const miningSummaryStatuses = {};
-const miningSummaries = {};
+/// TRANSACTIONS
 
-router.get("/mining-summary-status", asyncHandler(async (req, res, next) => {
-	const statusId = req.query.statusId;
-	if (statusId && miningSummaryStatuses[statusId]) {
-		res.json(miningSummaryStatuses[statusId]);
-
-		next();
-
-	} else {
-		res.json({});
-
-		next();
-	}
-}));
-
-router.get("/get-mining-summary", asyncHandler(async (req, res, next) => {
-	const statusId = req.query.statusId;
-
-	if (statusId && miningSummaries[statusId]) {
-		var summary = miningSummaries[statusId];
-		
-		res.json(summary);
-
-		next();
-
-		delete miningSummaries[statusId];
-		delete miningSummaryStatuses[statusId];
-
-	} else {
-		res.json({});
-
-		next();
-	}
-}));
-
-router.get("/build-mining-summary/:startBlock/:endBlock", asyncHandler(async (req, res, next) => {
-	try {
-		// long timeout
-		res.connection.setTimeout(600000);
-
-
-		var startBlock = parseInt(req.params.startBlock);
-		var endBlock = parseInt(req.params.endBlock);
-
-
-		const statusId = req.query.statusId;
-		if (statusId) {
-			miningSummaryStatuses[statusId] = {};
-		}
-
-		res.json({success:true, status:"started"});
-
-		next();
-		
-
-
-		var summary = await coreApi.buildMiningSummary(statusId, startBlock, endBlock, (update) => {
-			miningSummaryStatuses[statusId] = update;
-		});
-
-		// store summary until it's retrieved via /api/get-mining-summary
-		miningSummaries[statusId] = summary;
-
-	} catch (err) {
-		utils.logError("4328943ryh44", err);
-	}
-}));
-
-
-
-
-
-
-router.get("/mempool-tx-summaries/:txids", asyncHandler(async (req, res, next) => {
-	try {
-		const txids = req.params.txids.split(",").map(utils.asHash);
-
-		const promises = [];
-		const results = [];
-
-		for (var i = 0; i < txids.length; i++) {
-			const txid = txids[i];
-			const key = txid.substring(0, 6);
-
-			promises.push(new Promise(async (resolve, reject) => {
-				try {
-					const item = await coreApi.getMempoolTxDetails(txid, false);
-					const itemSummary = {
-						f: item.entry.fees.modified,
-						sz: item.entry.vsize ? item.entry.vsize : item.entry.size,
-						af: item.entry.fees.ancestor,
-						df: item.entry.fees.descendant,
-						dsz: item.entry.descendantsize,
-						t: item.entry.time,
-						w: item.entry.weight ? item.entry.weight : item.entry.size * 4
-					};
-
-					results.push(itemSummary);
-					
-					resolve();
-
-				} catch (e) {
-					utils.logError("38yereghee", e);
-
-					// resolve anyway
-					resolve();
-				}
-			}));
-		}
-
-		await Promise.all(promises);
-
-		res.json(results);
-
-		next();
-
-	} catch (err) {
-		res.json({success:false, error:err});
-
-		next();
-	}
-}));
-
-router.get("/raw-tx-with-inputs/:txid", function(req, res, next) {
+router.get("/tx/:txid", function(req, res, next) {
 	var txid = utils.asHash(req.params.txid);
 
 	var promises = [];
@@ -300,7 +113,7 @@ router.get("/raw-tx-with-inputs/:txid", function(req, res, next) {
 	promises.push(coreApi.getRawTransactionsWithInputs([txid]));
 
 	Promise.all(promises).then(function(results) {
-		res.json(results);
+		res.json(results[0].transactions[0]);
 
 		next();
 
@@ -311,81 +124,20 @@ router.get("/raw-tx-with-inputs/:txid", function(req, res, next) {
 	});
 });
 
-router.get("/block-tx-summaries/:blockHash/:blockHeight/:txids", function(req, res, next) {
-	var blockHash = req.params.blockHash;
-	var blockHeight = parseInt(req.params.blockHeight);
-	var txids = req.params.txids.split(",").map(utils.asHash);
-
-	var promises = [];
-
-	var results = [];
-
-	promises.push(new Promise(function(resolve, reject) {
-		coreApi.buildBlockAnalysisData(blockHeight, blockHash, txids, 0, results, resolve);
-	}));
-
-	Promise.all(promises).then(function() {
-		res.json(results);
-
-		next();
-
-	}).catch(function(err) {
-		res.json({success:false, error:err});
-
-		next();
-	});
-});
-
-router.get("/utils/:func/:params", function(req, res, next) {
-	var func = req.params.func;
-	var params = req.params.params;
-
-	var data = null;
-
-	if (func == "formatLargeNumber") {
-		if (params.indexOf(",") > -1) {
-			var parts = params.split(",");
-
-			data = utils.formatLargeNumber(parseInt(parts[0]), parseInt(parts[1]));
-
-		} else {
-			data = utils.formatLargeNumber(parseInt(params));
-		}
-	} else if (func == "formatCurrencyAmountInSmallestUnits") {
-		var parts = params.split(",");
-
-		data = utils.formatCurrencyAmountInSmallestUnits(new Decimal(parts[0]), parseInt(parts[1]));
-
-	} else {
-		data = {success:false, error:`Unknown function: ${func}`};
-	}
-
-	res.json(data);
-
-	next();
-});
 
 
 
-router.get("/v1/blocks/tip/height", function(req, res, next) {
-	rpcApi.getBlockCount().then(function(blockcount){
-		res.send(blockcount.toString());
-	}).catch(next);
-});
+/// BLOCKCHAIN
 
-router.get("/v1/blocks/tip/hash", function(req, res, next) {
-	coreApi.getBlockchainInfo().then(function(getblockchaininfo){
-		res.send(getblockchaininfo.bestblockhash.toString());
-	}).catch(next);
-});
-
-router.get("/v1/blocks/totalbtc", function(req, res, next) {	
+router.get("/blockchain/coins", function(req, res, next) {	
 	if (global.utxoSetSummary) {
 		var supply = parseFloat(global.utxoSetSummary.total_amount).toString();
+
 		res.send(supply.toString());
+
 		next();
-	}
-	else {
+
+	} else {
 		// estimated supply
 		coreApi.getBlockchainInfo().then(function(getblockchaininfo){
 			var estimatedSupply = utils.estimatedSupply(getblockchaininfo.blocks);
@@ -394,7 +146,12 @@ router.get("/v1/blocks/totalbtc", function(req, res, next) {
 	}
 });
 
-router.get("/v1/blocks/hashrate", function(req, res, next) {
+
+
+
+/// MINING
+
+router.get("/mining/hashrate", function(req, res, next) {
 	var blocksPerDay = 144;
 	var rates = [];
 	var timePeriods = [(1*blocksPerDay), (7*blocksPerDay), (30* blocksPerDay)];
@@ -411,13 +168,13 @@ router.get("/v1/blocks/hashrate", function(req, res, next) {
 				var hashRateData = utils.formatLargeNumber(info2, 1)
 				rates[2] = hashRateData[0];
 		
-				res.json({"1Day": rates[0]*1, "7Days": rates[1]*1, "30Days": rates[2]*1});
+				res.json({"1Day": rates[0]*1, "7Day": rates[1]*1, "30Day": rates[2]*1});
 			});
 		});
 	}).catch(next);
 });
 
-router.get("/v1/blocks/speed", asyncHandler(async (req, res, next) => {
+router.get("/mining/diff-adj-estimate", asyncHandler(async (req, res, next) => {
 	var promises = [];
 	const getblockchaininfo = await utils.timePromise("promises.api.getBlockchainInfo", coreApi.getBlockchainInfo());
 	var currentBlock;
@@ -440,49 +197,66 @@ router.get("/v1/blocks/speed", asyncHandler(async (req, res, next) => {
 	var firstBlockHeader = difficultyPeriodFirstBlockHeader;
 	var heightDiff = currentBlock.height - firstBlockHeader.height;
 	var timeDiff = currentBlock.mediantime - firstBlockHeader.mediantime;
-	var timePerBlock = timeDiff / heightDiff;	
+	var timePerBlock = timeDiff / heightDiff;
+	var dt = new Date().getTime() / 1000 - firstBlockHeader.time;	
+	var timePerBlock2 = dt / heightDiff;
 		
-	if (timePerBlock > 600) {
-			var diffAdjPercent = new Decimal(timeDiff / heightDiff / 600).times(100).minus(100);
-			diffAdjPercent = diffAdjPercent * -1;
+	if (timePerBlock2 > 600) {
+		var diffAdjPercent = new Decimal(dt / heightDiff / 600).times(100).minus(100);
+		diffAdjPercent = diffAdjPercent * -1;
 
 	} else {
-			var diffAdjPercent = new Decimal(100).minus(new Decimal(timeDiff / heightDiff / 600).times(100));
+		var diffAdjPercent = new Decimal(100).minus(new Decimal(dt / heightDiff / 600).times(100));
 	}	
 	
 	res.send(diffAdjPercent.toFixed(2).toString());
 }));
 
-router.get("/v1/mempool/count", function(req, res, next) {
+
+
+
+/// MEMPOOL
+
+router.get("/mempool/count", function(req, res, next) {
 	coreApi.getMempoolInfo().then(function(info){
 		res.send(info.size.toString());
 	}).catch(next);
 });
 
-router.get("/v1/mempool/fees/recommended", function(req, res, next) {
+router.get("/mempool/fees", function(req, res, next) {
 	var feeConfTargets = [1, 3, 6, 144];
 	coreApi.getSmartFeeEstimates("CONSERVATIVE", feeConfTargets).then(function(rawSmartFeeEstimates){
 		var smartFeeEstimates = {};
+		
 		for (var i = 0; i < feeConfTargets.length; i++) {
 			var rawSmartFeeEstimate = rawSmartFeeEstimates[i];
 			if (rawSmartFeeEstimate.errors) {
 				smartFeeEstimates[feeConfTargets[i]] = "?";
+
 			} else {
 				smartFeeEstimates[feeConfTargets[i]] = parseInt(new Decimal(rawSmartFeeEstimate.feerate).times(coinConfig.baseCurrencyUnit.multiplier).dividedBy(1000));
 			}
 		}		
 		
 		var results = {
-							"fastestFee":smartFeeEstimates[1],
-							"halfHourFee":smartFeeEstimates[3],
-							"hourFee":smartFeeEstimates[6],
-							"minimumFee":smartFeeEstimates[144]
-							};
+			"nextBlock":smartFeeEstimates[1],
+			"30min":smartFeeEstimates[3],
+			"60min":smartFeeEstimates[6],
+			"1day":smartFeeEstimates[144]
+		};
+
 		res.json(results);
+
 	}).catch(next);
 });
 
-router.get("/v1/price/:currency/moscowtime", function(req, res, next) {
+
+
+
+
+/// PRICE
+
+router.get("/price/:currency/moscowtime", function(req, res, next) {
 	var result = 0;
 	var amount = 1.0;
 	var currency = req.params.currency.toLowerCase();
@@ -505,7 +279,7 @@ router.get("/v1/price/:currency/moscowtime", function(req, res, next) {
 	next();
 });
 
-router.get("/v1/price/:currency/marketcap", function(req, res, next) {
+router.get("/price/:currency/marketcap", function(req, res, next) {
 	var result = 0;
 	
 	coreApi.getBlockchainInfo().then(function(getblockchaininfo){
@@ -528,18 +302,19 @@ router.get("/v1/price/:currency/marketcap", function(req, res, next) {
 		result = estimatedSupply * price;
 		res.send(result.toFixed(2).toString());
 		next();
+
 	}).catch(next);
 });
 
-router.get("/v1/price/:currency", function(req, res, next) {
+router.get("/price/:currency", function(req, res, next) {
 	var result = 0;
 	var amount = 1.0;
 	var currency = req.params.currency.toLowerCase();
+
 	if (global.exchangeRates != null && global.exchangeRates[currency] != null) {
 		var formatData = utils.formatExchangedCurrency(amount, currency);
 		result = formatData.val;
-	}
-	else if (currency == "xau" && global.exchangeRates != null && global.goldExchangeRates != null) {
+	} else if (currency == "xau" && global.exchangeRates != null && global.goldExchangeRates != null) {
 		var dec = new Decimal(amount);
 		dec = dec.times(global.exchangeRates.usd).dividedBy(global.goldExchangeRates.usd);
 		var exchangedAmt = parseFloat(Math.round(dec * 100) / 100).toFixed(2);
@@ -547,6 +322,30 @@ router.get("/v1/price/:currency", function(req, res, next) {
 	}
 	
 	res.send(result.toString());
+
+	next();
+});
+
+router.get("/price", function(req, res, next) {
+	var amount = 1.0;
+	var result = {};
+
+	["usd", "eur", "gbp", "xau"].forEach(currency => {
+		if (global.exchangeRates != null && global.exchangeRates[currency] != null) {
+			var formatData = utils.formatExchangedCurrency(amount, currency);
+			result[currency] = formatData.val;
+
+		} else if (currency == "xau" && global.exchangeRates != null && global.goldExchangeRates != null) {
+			var dec = new Decimal(amount);
+			dec = dec.times(global.exchangeRates.usd).dividedBy(global.goldExchangeRates.usd);
+			var exchangedAmt = parseFloat(Math.round(dec * 100) / 100).toFixed(2);
+			result[currency] = utils.addThousandsSeparators(exchangedAmt);
+		}
+	});
+	
+	
+	res.json(result);
+
 	next();
 });
 
