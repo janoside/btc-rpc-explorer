169c0416e1b4be4eb50a137d5ea7af92e0f5e568
gruve-p <jackielove4u@hotmail.com>
2021-08-08 14:09:15 +0200

    Tidy up quotes

--- app/coins/btcQuotes.js
+++ app/coins/btcQuotes.js
@@ -1,42 +1,10 @@
 module.exports = {
 	items: [
 		{
-			text: "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.",
-			speaker: "Satoshi",
-			url: "./block-height/0",
-			date: "2009-01-03",
-			context: "embedded in the Genesis block"
-		},
-		{
 			text: "Governments are good at cutting off the heads of centrally controlled networks like Napster, but pure P2P networks like Gnutella and Tor seem to be holding their own.",
 			speaker: "Satoshi",
 			date: "2008-11-07",
 			url: "https://www.mail-archive.com/cryptography@metzdowd.com/msg09971.html"
-		},
-		{
-			text: "The root problem with conventional currency is all the trust that's required to make it work.",
-			speaker: "Satoshi",
-			date: "2009-02-11",
-			url: "http://p2pfoundation.ning.com/forum/topics/bitcoin-open-source"
-		},
-		{
-			text: "The central bank must be trusted not to debase the currency, but the history of fiat currencies is full of breaches of that trust.",
-			speaker: "Satoshi",
-			date: "2009-02-11",
-			url: "http://p2pfoundation.ning.com/forum/topics/bitcoin-open-source"
-		},
-		{
-			text: "If you don't believe it or don't get it, I don't have the time to try to convince you, sorry.",
-			speaker: "Satoshi",
-			date: "2010-07-29",
-			url: "https://bitcointalk.org/index.php?topic=532.msg6306#msg6306",
-			context: "in response to a disagreement about 10-minute block settlement"
-		},
-		{
-			text: "The nature of Bitcoin is such that once version 0.1 was released, the core design was set in stone for the rest of its lifetime.",
-			speaker: "Satoshi",
-			date: "2010-06-17",
-			url: "https://bitcointalk.org/index.php?topic=195.msg1611#msg1611"
 		},
 		{
 			text: "Proof-of-work has the nice property that it can be relayed through untrusted middlemen.",
@@ -63,18 +31,6 @@ module.exports = {
 			url: "https://bitcointalk.org/index.php?topic=48.msg329#msg329"
 		},
 		{
-			text: "For greater privacy, it's best to use bitcoin addresses only once.",
-			speaker: "Satoshi",
-			date: "2009-11-25",
-			url: "https://bitcointalk.org/index.php?topic=8.msg34#msg34"
-		},
-		{
-			text: "The possibility to be anonymous or pseudonymous relies on you not revealing any identifying information about yourself in connection with the bitcoin addresses you use.",
-			speaker: "Satoshi",
-			date: "2009-11-25",
-			url: "https://bitcointalk.org/index.php?topic=8.msg34#msg34"
-		},
-		{
 			text: "We shouldn't be counting or spending transactions until they have at least 1 confirmation... unconfirmed transactions are very much second class citizens.",
 			speaker: "Satoshi",
 			date: "2010-09-30",
@@ -93,136 +49,13 @@ module.exports = {
 			url: "https://www.mail-archive.com/cryptography@metzdowd.com/msg09979.html"
 		},
 		{
-			text: "Writing a description for this thing for general audiences is bloody hard. There's nothing to relate it to.",
-			speaker: "Satoshi",
-			date: "2011-07-05",
-			url: "https://bitcointalk.org/index.php?topic=234.msg1976#msg1976",
-			context: "lamenting the difficulty of capturing the essence of Bitcoin succinctly"
-		},
-		{
-			text: "Nodes are not going to accept an invalid transaction as payment, and honest nodes will never accept a block containing them.",
-			speaker: "Satoshi",
-			date: "2008-10-31",
-			url: "https://bitcoin.org/bitcoin.pdf"
-		},
-		{
-			text: "With e-currency based on cryptographic proof, without the need to trust a third party middleman, money can be secure and transactions effortless.",
-			speaker: "Satoshi",
-			date: "2009-02-11",
-			url: "http://p2pfoundation.ning.com/forum/topics/bitcoin-open-source"
-		},
-		{
-			text: "Running bitcoin",
-			speaker: "Hal Finney",
-			date: "2009-01-10",
-			url: "https://twitter.com/halfin/status/1110302988",
-			context: "in a tweet announcing his experimentation with the Bitcoin software, as possibly the first person after Satoshi to run it"
-		},
-		{
-			text: "History shows it is not possible to insulate yourself from the consequences of others holding money that is harder than yours.",
-			speaker: "Saifedean Ammous",
-			date: "2018-04",
-			url: "https://saifedean.com/thebitcoinstandard/",
-			context: "from The Bitcoin Standard"
-		},
-		{
-			text: "The network is robust in its unstructured simplicity.",
-			speaker: "Satoshi",
-			date: "2008-10-31",
-			url: "https://bitcoin.org/bitcoin.pdf"
-		},
-		{
-			text: "Anybody who says they can’t afford #Bitcoin is missing the point.",
-			speaker: "Cedric Youngelman",
-			date: "2021-02-23",
-			url: "https://twitter.com/CedYoungelman/status/1364374324443033603"
-		},
-		{
-			text: "I like the idea of basing security on the assumption that the CPU power of honest participants outweighs that of the attacker. It is a very modern notion that exploits the power of the long tail.",
-			speaker: "Hal Finney",
-			date: "2008-11-08",
-			url: "https://www.mail-archive.com/cryptography@metzdowd.com/msg09975.html"
-		},
-		{
-			text: "Ultimately it’s good for the network for mining to be expensive. It makes it that much harder for a well financed attacker to dominate the network.",
-			speaker: "Hal Finney",
-			date: "2011-01",
-			url: "https://www.coindesk.com/hal-finney-bitcoin-words"
-		},
-		{
-			text: "I see Bitcoin as ultimately becoming a reserve currency for banks...",
-			speaker: "Hal Finney",
-			date: "2010-12",
-			url: "https://www.coindesk.com/hal-finney-bitcoin-words"
-		},
-		{
-			text: "For Bitcoin to succeed and become secure, bitcoins must become vastly more expensive.",
-			speaker: "Hal Finney",
-			date: "2011-03",
-			url: "https://www.coindesk.com/hal-finney-bitcoin-words"
-		},
-		{
-			text: "I am very intrigued by Bitcoin. It has all the signs. Paradigm shift, hackers love it, yet it is described as a toy. Just like microcomputers.",
-			speaker: "Paul Graham",
-			date: "2013-03-18",
-			url: "https://news.ycombinator.com/item?id=5402513"
-		},
-		{
-			text: "Bitcoin may be the TCP/IP of money.",
-			speaker: "Paul Buchheit",
-			date: "2013-04-29",
-			url: "https://www.reddit.com/r/Bitcoin/comments/1dd3uv/paul_buchheit_inventor_of_gmail_tweets_how/"
-		},
-		{
-			text: "The twin pillars of Bitcoin's value proposition: (1) It's possible to secure and transport your wealth without anyone's permission (2) Your wealth cannot be debased. Everything else is noise.",
-			speaker: "Vijay Boyapati",
-			date: "2019-07-02",
-			url: "https://twitter.com/real_vijay/status/1146190588707868678"
-		},
-		{
-			text: "By attaching energy to a block, we give it 'form', allowing it to have real weight & consequences in the physical world.",
-			speaker: "https://twitter.com/hugohanoi",
-			date: "2018-02-10",
-			url: "https://bitcointechtalk.com/the-anatomy-of-proof-of-work-98c85b6f6667"
-		},
-		{
 			text: "The project needs to grow gradually so the software can be strengthened along the way.",
 			speaker: "Satoshi",
 			date: "2010-12-05",
 			url: "https://bitcointalk.org/index.php?topic=1735.msg26999#msg26999"
 		},
 		{
-			text: "#bitcoin blockchain is the new wonder of the world, more work and human ingenuity, than went into the great pyramids of Egypt. The biggest computation ever done, a digital monument, a verifable artefact of digital gold - the foundation of a new digital age",
-			speaker: "Adam Back",
-			date: "2020-06-04",
-			url: "https://twitter.com/adam3us/status/1268492893414711297"
-		},
-		{
-			text: "[Bitcoin is] more typical of a precious metal. Instead of the supply changing to keep the value the same, the supply is predetermined and the value changes.",
-			speaker: "Satoshi",
-			date: "2009-02-18",
-			url: "http://p2pfoundation.ning.com/xn/detail/2003008:Comment:9562"
-		},
-		{
-			text: "The steady addition of a constant of amount of new coins is analogous to gold miners expending resources to add gold to circulation. In our case, it is CPU time and electricity that is expended.",
-			speaker: "Satoshi",
-			date: "2008-10-31",
-			url: "https://bitcoin.org/bitcoin.pdf"
-		},
-		{
-			text: "It might make sense just to get some in case it catches on. If enough people think the same way, that becomes a self fulfilling prophecy.",
-			speaker: "Satoshi",
-			date: "2009-01-16",
-			url: "https://www.metzdowd.com/pipermail/cryptography/2009-January/015014.html"
-		},
-		{
 			text: "I'm sure that in 20 years there will either be very large transaction volume or no volume.",
-			speaker: "Satoshi",
-			date: "2010-02-14",
-			url: "https://bitcointalk.org/index.php?topic=48.msg329#msg329"
-		},
-		{
-			text: "In a few decades when the reward gets too small, the transaction fee will become the main compensation for nodes.",
 			speaker: "Satoshi",
 			date: "2010-02-14",
 			url: "https://bitcointalk.org/index.php?topic=48.msg329#msg329"
@@ -232,100 +65,6 @@ module.exports = {
 			speaker: "Satoshi",
 			date: "2010-06-21",
 			url: "https://bitcointalk.org/index.php?topic=198.msg1647#msg1647"
-		},
-		{
-			text: "The utility of the exchanges made possible by Bitcoin will far exceed the cost of electricity used. Therefore, not having Bitcoin would be the net waste.",
-			speaker: "Satoshi",
-			date: "2010-08-07",
-			url: "https://bitcointalk.org/index.php?topic=721.msg8114#msg8114"
-		},
-		{
-			text: "If nuclear war destroyed half of our planet, [Bitcoin] would continue to live, uncorrupted.",
-			speaker: "Ralph Merkle",
-			date: "2021-03-12",
-			url: "https://merkle.com/papers/DAOdemocracyDraft.pdf"
-		},
-		{
-			text: "No one has found the bottom of the Bitcoin rabbit hole.",
-			speaker: "Jameson Lopp",
-			date: "2010-11-10",
-			url: "https://twitter.com/lopp/status/1061415918616698881"
-		},
-		{
-			text: "I don't think there is anything more important in my lifetime to work on [than Bitcoin].",
-			speaker: "Jack Dorsey",
-			date: "2021-06-05",
-			url: "https://youtu.be/rSSnyJpFNZU?t=62",
-			context: "at the Bitcoin 2021 conference; in 'Banking the Unbanked'"
-		},
-		{
-			text: "Bitcoin changes absolutely everything.",
-			speaker: "Jack Dorsey",
-			date: "2021-06-05",
-			url: "https://youtu.be/rSSnyJpFNZU?t=39",
-			context: "at the Bitcoin 2021 conference; in 'Banking the Unbanked'"
-		},
-		{
-			text: "For the first time in human history, we can grant property rights to 8 billion people.",
-			speaker: "Michael Saylor",
-			url: "https://youtu.be/_uJwvlimnNs?t=378",
-			date: "2021-06-05",
-			context: "at the Bitcoin 2021 conference; in the Fireside chat with Max Keiser"
-		},
-		{
-			text: "We don't need the banks anymore.",
-			speaker: "Jack Dorsey",
-			date: "2021-06-05",
-			url: "https://youtu.be/rSSnyJpFNZU?t=198",
-			context: "at the Bitcoin 2021 conference; in 'Banking the Unbanked'"
-		},
-		{
-			text: "#Bitcoin is the one thing that can't be shutdown. A beacon of freedom in a benighted world.",
-			speaker: "Vijay Boyapati",
-			date: "2021-06-05",
-			url: "https://twitter.com/real_vijay/status/1401145456361607172"
-		},
-		{
-			text: "There is nothing else that compares to Bitcoin",
-			speaker: "Jack Dorsey",
-			date: "2021-06-05",
-			url: "https://twitter.com/DocumentingBTC/status/1401145331966959627"
-		},
-		{
-			text: "I AM HODLING",
-			speaker: "GameKyuubi",
-			date: "2013-12-18",
-			url: "https://bitcointalk.org/index.php?topic=375643.0"
-		},
-		{
-			text: "Bitcoin endures because the engineers that build it expect the unexpected. Money is a dangerous game, and any weaknesses, no matter how small, will be exploited.",
-			speaker: "Neil Woodfine",
-			date: "2020-01-06",
-			url: "https://twitter.com/nwoodfine/status/1214240205768495109"
-		},
-		{
-			text: "I had to write all the code before I could convince myself that I could solve every problem, then I wrote the paper.",
-			speaker: "Satoshi",
-			date: "2008-11-08",
-			url: "https://www.metzdowd.com/pipermail/cryptography/2008-November/014832.html"
-		},
-		{
-			text: "Bitcoin is radically changing the world by being unchangeable.",
-			speaker: "Robert Breedlove",
-			date: "2020-05-21",
-			url: "https://twitter.com/Breedlove22/status/1263520680944910337"
-		},
-		{
-			text: "There has never been a government that didn’t sooner or later try to reduce the freedom of its subjects and gain more control over them, and there probably will never be one. Therefore, instead of trying to convince our current government not to try, we’ll develop the technology that will make it impossible for the government to succeed.",
-			speaker: "Wei Dai",
-			date: "1995-02",
-			url: "https://www.cato.org/cato-journal/spring/summer-2021/financial-freedom-privacy-post-cash-world#freedom-privacy-through-technology"
-		},
-		{
-			text: "Bitcoin is a once in a thousand year monetary innovation and it's too late to ignore that fact.",
-			speaker: "Nik Bhatia",
-			date: "2021-07-10",
-			url: "https://twitter.com/BitcoinSapiens/status/1413966460997840899"
 		},
 		{
 			text: "Being open source means anyone can independently review the code. If it was closed source, nobody could verify the security. I think it's essential for a program of this nature to be open source.",
@@ -344,106 +83,7 @@ module.exports = {
 			speaker: "Satoshi",
 			date: "2009-12-12",
 			url: "https://bitcointalk.org/index.php?topic=13.msg46#msg46"
-		},
-		{
-			text: "There are more coming.",
-			speaker: "Satoshi",
-			date: "2009-12-12",
-			context: "discussing new exchanges and payment systems joining the network",
-			url: "https://bitcointalk.org/index.php?topic=13.msg46#msg46"
-		},
-		{
-			text: "For people living under authoritarian governments, Bitcoin can be a valuable financial tool",
-			speaker: "Alex Gladstein",
-			date: "2018-12-28",
-			url: "https://time.com/5486673/bitcoin-venezuela-authoritarian/"
-		},
-		{
-			text: "[Bitcoin is] a claim on the future productivity of the planet, in perpetuity, discounted to the present.",
-			speaker: "Trace Mayer",
-			date: "2019-10-09",
-			url: "https://stephanlivera.com/episode/115/"
-		},
-		{
-			text: "First step to understanding Bitcoin: admitting you don't understand Bitcoin.",
-			speaker: "Jameson Lopp",
-			date: "2017-02-08",
-			url: "https://twitter.com/lopp/status/829419398708486145"
-		},
-		{
-			text: "...the price gyrations of bitcoins are entertaining to me.",
-			speaker: "Hal Finney",
-			date: "2013-03-19",
-			url: "https://bitcointalk.org/index.php?topic=155054.msg1643833#msg1643833"
-		},
-		{
-			text: "I ... do think that there is potential value in a form of unforgeable token whose production rate is predictable and can't be influenced by corrupt parties.",
-			speaker: "Hal Finney",
-			date: "2008-11-08",
-			url: "https://www.mail-archive.com/cryptography@metzdowd.com/msg09975.html"
-		},
-		{
-			text: "This thing is not a toy anymore, and it’s maybe not an asset anymore as well. It is going to be much bigger than that.",
-			date: "2020-05-01",
-			speaker: "Plan B",
-			url: "https://twitter.com/WhatBitcoinDid/status/1256206584171999233"
-		},
-		{
-			text: "There is no capacity to kill Bitcoin.",
-			date: "2019-07-17",
-			speaker: "Patrick McHenry (US Representative)",
-			url: "https://youtu.be/xolYGw2wU6Y?t=232"
-		},
-		{
-			text: "The more loathsome people that use bitcoin, the better. The more it works even if teeming with people that mutually despise each other, the more faith I have in its ability to protect us all",
-			speaker: "Nic Carter",
-			date: "2020-09-07",
-			url: "https://twitter.com/nic__carter/status/1303100515333672961"
-		},
-		{
-			text: "Bitcoin is a way out of our current system, which is exclusionary and unjust, and tends to prey on the weak and disenfranchised while letting the corrupt Davos crowd walk free.",
-			speaker: "Alex Gladstein",
-			date: "2019-03-13",
-			url: "https://www.coincenter.org/a-human-rights-activists-response-to-bitcoin-critics/"
-		},
-		{
-			text: "Decentralized and private payments are a necessary innovation for a digital future where we retain our civil liberties and personal freedoms.",
-			speaker: "Alex Gladstein",
-			date: "2019-06-26",
-			url: "https://bitcoinmagazine.com/culture/op-ed-as-cash-fades-will-bitcoin-keep-protest-alive-in-the-surveillance-age"
-		},
-		{
-			text: "One of the greatest things that Satoshi did was disappear.",
-			speaker: "Jimmy Song",
-			url: "https://jimmysong.medium.com/why-bitcoin-is-different-e17b813fd947",
-			date: "2018-04-02"
-		},
-		{
-			text: "Decentralized digital scarcity is the real innovation... Bitcoin was the first, and... continues to be the only such coin.",
-			speaker: "Jimmy Song",
-			url: "https://jimmysong.medium.com/why-bitcoin-is-different-e17b813fd947",
-			date: "2018-04-02"
 		}
-		/*{
-			text: "Bitcoin is a technological tour de force.",
-			speaker: "Bill Gates",
-			url: "https://paybis.com/blog/cryptocurrency-quotes/"
-		},*/
-		/*{
-			text: "The whole point of doing ICOs is to get other people's bitcoins.",
-			speaker: "Pierre Rochard",
-			url: "https://twitter.com/btc_quotes/status/1396194347419308035"
-		},*/
-		/*{
-			text: "Bitcoin is a game where everyone watches everyone else to make sure nobody cheats.",
-			speaker: "Jameson Lopp",
-			url: "https://twitter.com/Bitcoin_Sam/status/1401545443100663823"
-		},*/
-		/*{
-			text: "Buying bitcoin is the most powerful protest an individual can make against the current economic system.",
-			speaker: "https://twitter.com/LucDossis",
-			url: "https://twitter.com/btc_quotes/status/1410146340378644484?s=20"
-		},*/
-		
+
 	]
 };
