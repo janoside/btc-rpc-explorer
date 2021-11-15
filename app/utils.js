bd1f5d67fae5b40272ec08ef51f0dc5dc7ac1b26
Dan Janosik <dan@47.io>
2021-11-15 11:00:04 -0500

    better formatting for hex content

--- app/utils.js
+++ app/utils.js
@@ -153,12 +153,7 @@ function redirectToConnectPageIfNeeded(req, res) {
 }
 
 function hex2ascii(hex) {
-	var str = "";
-	for (var i = 0; i < hex.length; i += 2) {
-		str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-	}
-	
-	return str;
+	return Buffer.from(hex, "hex").toString("utf8");
 }
 
 function splitArrayIntoChunks(array, chunkSize) {
