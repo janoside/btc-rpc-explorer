17cbb96bc143ad5d275125c4157a310dfd0bfadd
Dan Janosik <dan@47.io>
2023-03-21 15:25:09 -0400

    add eslint; shrinkwrap; bump min node.js version in readme (required by update to lru-cache)

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
