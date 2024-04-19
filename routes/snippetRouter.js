4fc15aa9a93fbfddfaeb88c13b801be138c2f196
Dan Janosik <dan@47.io>
2024-04-19 11:51:18 -0400

    homepage: live halving countdown section when very close (98%+)

--- routes/snippetRouter.js
+++ routes/snippetRouter.js
@@ -84,6 +84,59 @@ router.get("/next-block", asyncHandler(async (req, res, next) => {
 	res.render("snippets/index-next-block");
 }));
 
+router.get("/index-halving-countdown", asyncHandler(async (req, res, next) => {
+	try {
+		const getblockchaininfo = await utils.timePromise("snippet.index-halving-countdown.getBlockchainInfo", async () => {
+			return await coreApi.getBlockchainInfo();
+		});
+
+		let promises = [];
+
+		res.locals.getblockchaininfo = getblockchaininfo;
+		res.locals.difficultyPeriod = parseInt(Math.floor(getblockchaininfo.blocks / coinConfig.difficultyAdjustmentBlockCount));
+
+		let blockHeights = [];
+		if (getblockchaininfo.blocks) {
+			for (let i = 0; i < 1; i++) {
+				blockHeights.push(getblockchaininfo.blocks - i);
+			}
+		} else if (global.activeBlockchain == "regtest") {
+			// hack: default regtest node returns getblockchaininfo.blocks=0, despite
+			// having a genesis block; hack this to display the genesis block
+			blockHeights.push(0);
+		}
+
+		promises.push(utils.timePromise("snippet.index-halving-countdown.getBlockHeaderByHeight", async () => {
+			let h = coinConfig.difficultyAdjustmentBlockCount * res.locals.difficultyPeriod;
+			res.locals.difficultyPeriodFirstBlockHeader = await coreApi.getBlockHeaderByHeight(h);
+		}));
+
+		promises.push(utils.timePromise("snippet.index-halving-countdown.getBlocksByHeight", async () => {
+			const latestBlocks = await coreApi.getBlocksByHeight(blockHeights);
+			
+			res.locals.latestBlocks = latestBlocks;
+		}));
+
+		await utils.awaitPromises(promises);
+
+
+		let nextHalvingData = utils.nextHalvingEstimates(res.locals.difficultyPeriodFirstBlockHeader, res.locals.latestBlocks[0]);
+
+		res.locals.nextHalvingData = nextHalvingData;
+
+		await utils.timePromise("snippet.index-halving-countdown.render", async () => {
+			res.render("snippets/index-halving-countdown");
+		});
+
+	} catch (e) {
+		res.locals.pageErrors.push(utils.logError("390wrgehburfuge", e));
+
+		await utils.timePromise("snippet.index-halving-countdown.render", async () => {
+			res.render("snippets/index-halving-countdown");
+		});
+	}
+}));
+
 router.get("/utxo-set", asyncHandler(async (req, res, next) => {
 	const promises = [];
 
