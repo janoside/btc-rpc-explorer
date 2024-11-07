04636234633b6554cd72ea6d9cba3e6e89f78535
Dan Janosik <dan@47.io>
2024-11-07 15:58:39 -0500

    openai bot

--- app/utils.js
+++ app/utils.js
@@ -61,7 +61,8 @@ const crawlerBotUserAgentStrings = {
 	"moz": new RegExp("dotbot", "i"),
 	"semrush": new RegExp("SemrushBot", "i"),
 	"majestic": new RegExp("MJ12bot", "i"),
-	"python-requests": new RegExp("python-requests", "i")
+	"python-requests": new RegExp("python-requests", "i"),
+	"openai": new RegExp("OAI-SearchBot", "i"),
 };
 
 const ipMemoryCache = {};
